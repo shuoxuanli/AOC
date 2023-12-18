0366faa8a751dd803b58223dc120a753032a4258
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-12-18 15:25:49 -0500

    Create p1.py

---
+++ 2023/day08/p1.py
@@ -0,0 +1,15 @@
+import sys
+
+dir, lines = sys.stdin.read().split("\n\n")
+maps = [x.split(" = ") for x in lines.split("\n")]
+maps = {a : b[1 : -1].split(", ") for a, b in maps}
+
+cur = "AAA"
+p, ans = 0, 0
+
+while cur != "ZZZ":
+	cur = maps[cur][dir[p] == 'R']
+	ans += 1
+	p = (p + 1) % len(dir)
+
+print(ans)
