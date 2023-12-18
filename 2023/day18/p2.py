9b08ddebdb52a8d4845e78ae530eeb5f883a0df2
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-12-18 15:47:00 -0500

    Create p2.py

---
+++ 2023/day18/p2.py
@@ -0,0 +1,30 @@
+import sys
+
+lines = sys.stdin.read().split("\n")
+
+p = 0
+x, y = 0, 0
+pt = [(x, y)]
+for line in lines:
+	a, b, c = line.split()
+	b = int(c[2 : -2], 16)
+	a = int(c[-2], 16)
+	p += b
+	if a == 0:
+		y += b
+	elif a == 1:
+		x += b
+	elif a == 2:
+		y -= b
+	else:
+		x -= b
+	pt.append((x, y))
+
+ans = 0
+for i in range(len(pt)):
+	x1, y1 = pt[i]
+	x2, y2 = pt[(i + 1) % len(pt)]
+	ans += x1 * y2 - x2 * y1
+ans = abs(ans) // 2 + p // 2 + 1
+
+print(ans)
