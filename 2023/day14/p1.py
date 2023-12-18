af7d9f81e7c11fb31e77b8aa94677a1f8a70a480
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-12-18 15:42:49 -0500

    Create p1.py

---
+++ 2023/day14/p1.py
@@ -0,0 +1,19 @@
+import sys
+
+g = sys.stdin.read().split("\n")
+
+n, m = len(g), len(g[0])
+
+g = [list(s) for s in g]
+
+ans = 0
+for j in range(m):
+	for i in range(n):
+		if g[i][j] == 'O':
+			x = i
+			while x > 0 and g[x - 1][j] == '.':
+				g[x][j], g[x - 1][j] = g[x - 1][j], g[x][j]
+				x -= 1
+			ans += n - x
+
+print(ans)
