ee5a24a64223d64a8c5c8a269991eb946adff7ec
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-12-18 15:44:16 -0500

    Update p1.py

--- 2023/day15/p1.py
+++ 2023/day15/p1.py
@@ -1,33 +1,11 @@
 import sys
 
-g = sys.stdin.read().split("\n")
+line = sys.stdin.read().split(",")
 
-n, m = len(g), len(g[0])
+def hash(s):
+	h = 0
+	for x in s:
+		h = (h + ord(x)) * 17 % 256
+	return h
 
-ch = [[0, 1], [-1, 0], [0, -1], [1, 0]]
-vis = [[[False for k in range(4)] for j in range(m)] for i in range(n)]
-
-q = [(0, -1, 0)]
-
-while q != []:
-	x, y, d = q.pop(0)
-	dx, dy = ch[d]
-	nd = []
-	nx, ny = x + dx, y + dy
-	if nx >= 0 and nx < n and ny >= 0 and ny < m:
-		if d % 2 == 0 and g[nx][ny] == '|':
-			nd = [(d + 1) % 4, (d + 3) % 4]
-		if d % 2 == 1 and g[nx][ny] == '-':
-			nd = [(d + 1) % 4, (d + 3) % 4]
-		if g[nx][ny] == '/':
-			nd = [(d + 1) % 4 if d % 2 == 0 else (d + 3) % 4]
-		if g[nx][ny] == '\\':
-			nd = [(d + 3) % 4 if d % 2 == 0 else (d + 1) % 4]
-		if nd == []: nd = [d]
-
-		for ds in nd:
-			if not vis[nx][ny][ds]:
-				vis[nx][ny][ds] = True
-				q.append((nx, ny, ds))
-
-print(sum(vis[i][j][0] or vis[i][j][1] or vis[i][j][2] or vis[i][j][3] for i in range(n) for j in range(m)))
+print(sum(map(hash, line)))
