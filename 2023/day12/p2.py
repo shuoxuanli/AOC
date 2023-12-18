1f60cfbd54b11775539c01f5a538fc180806d148
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-12-18 15:41:54 -0500

    Create p2.py

---
+++ 2023/day12/p2.py
@@ -0,0 +1,27 @@
+import sys
+
+lines = sys.stdin.read().split("\n")
+	
+ans = 0
+for line in lines:
+	s, a = line.split()
+	a = map(int, a.split(','))
+	a *= 5
+	s = ((s + '?') * 5)[:-1]
+	n, m = len(s), len(a)
+	dp = [[[0 for k in range(2)] for j in range(m + 1)] for i in range(n + 1)]
+	dp[0][0][0] = 1
+	for i in range(n):
+		for j in range(m + 1):
+			ok = True
+			if s[i] != '#':
+				dp[i + 1][j][0] += dp[i][j][0]
+				dp[i + 1][j][0] += dp[i][j][1]
+			if j < m:
+				for k in range(a[j]):
+					ok = ok and i + k < n and s[i + k] != '.'
+				if ok:
+					dp[i + a[j]][j + 1][1] += dp[i][j][0]
+	ans += dp[n][m][0] + dp[n][m][1]
+
+print(ans)
