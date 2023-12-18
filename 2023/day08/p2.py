9fb65f6b83c7ccbccc18309cb5e4d878c3bdbb82
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-12-18 15:26:09 -0500

    Create p2.py

---
+++ 2023/day08/p2.py
@@ -0,0 +1,19 @@
+import sys
+from math import lcm
+
+dir, lines = sys.stdin.read().split("\n\n")
+maps = [x.split(" = ") for x in lines.split("\n")]
+maps = {a : b[1 : -1].split(", ") for a, b in maps}
+
+ans = []
+for cur in maps:
+	if cur[-1] != 'A':
+		continue
+	p, cnt = 0, 0
+	while cur[-1] != 'Z':
+		cur = maps[cur][dir[p] == 'R']
+		cnt += 1
+		p = (p + 1) % len(dir)
+	ans.append(cnt)
+
+print(lcm(*ans))
