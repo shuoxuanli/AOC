9cb3c6e126d1d649784edeca790a8110740dc99c
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-12-18 15:23:52 -0500

    Create p2.py

---
+++ 2023/day06/p2.py
@@ -0,0 +1,16 @@
+import sys
+
+lines = sys.stdin.read().split("\n")
+
+a = map(int, lines[0].split(":")[1].split())
+b = map(int, lines[1].split(":")[1].split())
+
+sa = int(''.join(map(str, a)))
+sb = int(''.join(map(str, b)))
+
+ans = 0
+for i in range(sa + 1):
+	if i * (sa - i) > sb:
+		ans += 1
+
+print(ans)
