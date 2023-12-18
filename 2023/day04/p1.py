031b992b756c486a4dcbe74b2db06a1a4093765c
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-12-18 15:21:53 -0500

    Create p1.py

---
+++ 2023/day04/p1.py
@@ -0,0 +1,13 @@
+import sys
+
+lines = sys.stdin.read().split("\n")
+
+ans = 0
+for line in lines:
+	line = line.split(": ")[1]
+	a, b = line.split("|")
+	a, b = a.split(), b.split()
+	s = sum(x in a for x in b)
+	if s > 0: ans += 2 ** (s - 1)
+
+print(ans)
