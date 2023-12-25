925f673447827a6d06988c7dc323512f8103abba
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-12-25 23:13:35 +0900

    Update p1.py

--- 2023/day06/p1.py
+++ 2023/day06/p1.py
@@ -7,10 +7,10 @@ b = map(int, lines[1].split(":")[1].split())
 
 ans = 1
 for i in range(len(a)):
-	ways = 0
-	for j in range(a[i] + 1):
-		if j * (a[i] - j) > b[i]:
-			ways += 1
-	ans *= ways
+    ways = 0
+    for j in range(a[i] + 1):
+        if j * (a[i] - j) > b[i]:
+            ways += 1
+    ans *= ways
 
 print(ans)
