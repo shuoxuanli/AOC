11f7384bfc46377ec1ac186deaaa53759c668229
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-12-25 23:34:31 +0900

    Update p1.py

--- 2023/day21/p1.py
+++ 2023/day21/p1.py
@@ -11,15 +11,15 @@ s = set((x, y, 64))
 
 ans = 0
 while q:
-	x, y, d = q.pop(0)
-	if d == 0:
-		ans += 1
-		continue
-	for dx, dy in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
-		nx, ny = x + dx, y + dy
-		if 0 <= nx < n and 0 <= ny < m and g[nx][ny] != '#':
-			if (nx, ny, d - 1) not in s:
-				s.add((nx, ny, d - 1))
-				q.append((nx, ny, d - 1))
+    x, y, d = q.pop(0)
+    if d == 0:
+        ans += 1
+        continue
+    for dx, dy in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < n and 0 <= ny < m and g[nx][ny] != '#':
+            if (nx, ny, d - 1) not in s:
+                s.add((nx, ny, d - 1))
+                q.append((nx, ny, d - 1))
 
 print(ans)
