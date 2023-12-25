b196d9f1c9c67c30900a74f64edc8649e7cb510a
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-12-25 23:26:09 +0900

    Update p1.py

--- 2023/day13/p1.py
+++ 2023/day13/p1.py
@@ -4,26 +4,26 @@ notes = sys.stdin.read().split("\n\n")
 
 x, y = 0, 0
 for g in notes:
-	g = g.split("\n")
-	n, m = len(g), len(g[0])
-	for i in range(n - 1):
-		ok = True
-		for j in range(m):
-			l, r = i, i + 1
-			while l >= 0 and r < n and ok:
-				ok = ok and g[l][j] == g[r][j]
-				l -= 1
-				r += 1
-		if ok: x += i + 1
+    g = g.split("\n")
+    n, m = len(g), len(g[0])
+    for i in range(n - 1):
+        ok = True
+        for j in range(m):
+            l, r = i, i + 1
+            while l >= 0 and r < n and ok:
+                ok = ok and g[l][j] == g[r][j]
+                l -= 1
+                r += 1
+        if ok: x += i + 1
 
-	for i in range(m - 1):
-		ok = True
-		for j in range(n):
-			l, r = i, i + 1
-			while l >= 0 and r < m and ok:
-				ok = ok and g[j][l] == g[j][r]
-				l -= 1
-				r += 1
-		if ok: y += i + 1
+    for i in range(m - 1):
+        ok = True
+        for j in range(n):
+            l, r = i, i + 1
+            while l >= 0 and r < m and ok:
+                ok = ok and g[j][l] == g[j][r]
+                l -= 1
+                r += 1
+        if ok: y += i + 1
 
 print(x * 100 + y)
