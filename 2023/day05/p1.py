06c3a0a4050354afb9dd2b57db64555bbdf43d0b
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-12-25 23:12:53 +0900

    Update p1.py

--- 2023/day05/p1.py
+++ 2023/day05/p1.py
@@ -6,12 +6,12 @@ num, lines = lines[0], lines[1:]
 num = map(int, num.split(":")[1].split())
 
 for line in lines:
-	line = line.split(":\n")[1].split("\n")
-	r = [map(int, v.split()) for v in line]
-	for i in range(len(num)):
-		for a, b, c in r:
-			if num[i] >= b and num[i] <= b + c - 1:
-				num[i] = a + num[i] - b
-				break
+    line = line.split(":\n")[1].split("\n")
+    r = [map(int, v.split()) for v in line]
+    for i in range(len(num)):
+        for a, b, c in r:
+            if num[i] >= b and num[i] <= b + c - 1:
+                num[i] = a + num[i] - b
+                break
 
 print(min(num))
