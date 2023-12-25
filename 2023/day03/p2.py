16fc274537f13e7016922bbd83b122b9b9bf560f
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-12-25 23:12:05 +0900

    Update p2.py

--- 2023/day03/p2.py
+++ 2023/day03/p2.py
@@ -1,13 +1,31 @@
 import sys
 
-lines = sys.stdin.read().split("\n")
+g = sys.stdin.read().split("\n")
+
+n, m = len(g), len(g[0])
+
+s = []
+for i in range(n):
+    num = 0
+    for j in range(m + 1):
+        if j < m and g[i][j].isdigit():
+            num = num * 10 + int(g[i][j])
+        else:
+            if num != 0:
+                s.append([i, j - len(str(num)), j - 1, num])
+                num = 0
 
 ans = 0
-for line in lines:
-    line = line.split(": ")[1]
-    a, b = line.split("|")
-    a, b = a.split(), b.split()
-    s = sum(x in a for x in b)
-    if s > 0: ans += 2 ** (s - 1)
+for i in range(n):
+    for j in range(m):
+        if g[i][j] == '*':
+            lis = []
+            for k in range(-1, 2):
+                for l in range(-1, 2):
+                    for p in range(len(s)):
+                        r, x, y, z = s[p]
+                        if r == i + k and x <= j + l <= y and p not in lis:
+                            lis.append(p)
+            if len(lis) == 2: ans += s[lis[0]][3] * s[lis[1]][3]
 
 print(ans)
