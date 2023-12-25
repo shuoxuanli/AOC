af599f5ae2378f85dfad8bee2280734c08187ca4
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-12-25 23:16:21 +0900

    Update p2.py

--- 2023/day08/p2.py
+++ 2023/day08/p2.py
@@ -7,13 +7,13 @@ maps = {a : b[1 : -1].split(", ") for a, b in maps}
 
 ans = []
 for cur in maps:
-	if cur[-1] != 'A':
-		continue
-	p, cnt = 0, 0
-	while cur[-1] != 'Z':
-		cur = maps[cur][dir[p] == 'R']
-		cnt += 1
-		p = (p + 1) % len(dir)
-	ans.append(cnt)
+    if cur[-1] != 'A':
+        continue
+    p, cnt = 0, 0
+    while cur[-1] != 'Z':
+        cur = maps[cur][dir[p] == 'R']
+        cnt += 1
+        p = (p + 1) % len(dir)
+    ans.append(cnt)
 
 print(lcm(*ans))
