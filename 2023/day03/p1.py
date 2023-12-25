7dfb119fb274a6c468226ad13f074581827bb357
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-12-25 23:01:31 +0900

    Update p1.py

--- 2023/day03/p1.py
+++ 2023/day03/p1.py
@@ -1,28 +1,29 @@
 import sys
 
-lines = sys.stdin.read().split("\n")
+g = sys.stdin.read().split("\n")
+
+n, m = len(g), len(g[0])
 
 def check(i, j):
-	for x in range(-1, 2):
-		for y in range(-1, 2):
-			ni = i + x
-			nj = j + y
-			if ni >= 0 and ni < len(lines) and nj >= 0 and nj < len(lines[0]):
-				if not lines[ni][nj].isdigit() and lines[ni][nj] != '.':
-					return True
-	return False
+    for x in range(-1, 2):
+        for y in range(-1, 2):
+            ni, nj = i + x, j + y
+            if 0 <= ni < n and 0 <= nj < m:
+                if not g[ni][nj].isdigit() and g[ni][nj] != '.':
+                    return True
+    return False
 
 ans = 0
-for i in range(len(lines)):
-	num = 0
-	adj = False;
-	for j in range(len(lines[i]) + 1):
-		if j < len(lines[i]) and lines[i][j].isdigit():
-			adj = adj or check(i, j)
-			num = num * 10 + int(lines[i][j])
-		else:
-			if adj: ans += num
-			adj = False
-			num = 0
+for i in range(n):
+    num = 0
+    adj = False;
+    for j in range(m + 1):
+        if j < m and g[i][j].isdigit():
+            adj = adj or check(i, j)
+            num = num * 10 + int(g[i][j])
+        else:
+            if adj: ans += num
+            adj = False
+            num = 0
 
 print(ans)
