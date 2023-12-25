23f4d3a1438b1030983f2b77f2b06cf87a4e7e9d
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-12-25 23:33:11 +0900

    Update p2.py

--- 2023/day18/p2.py
+++ 2023/day18/p2.py
@@ -6,25 +6,25 @@ pt = [(x, y)]
 p, x, y = 0, 0, 0
 
 for line in lines:
-	a, b, c = line.split()
-	b = int(c[2 : -2], 16)
-	a = int(c[-2], 16)
-	p += b
-	if a == 0:
-		y += b
-	elif a == 1:
-		x += b
-	elif a == 2:
-		y -= b
-	else:
-		x -= b
-	pt.append((x, y))
+    a, b, c = line.split()
+    b = int(c[2 : -2], 16)
+    a = int(c[-2], 16)
+    p += b
+    if a == 0:
+        y += b
+    elif a == 1:
+        x += b
+    elif a == 2:
+        y -= b
+    else:
+        x -= b
+    pt.append((x, y))
 
 ans = 0
 for i in range(len(pt)):
-	x1, y1 = pt[i]
-	x2, y2 = pt[(i + 1) % len(pt)]
-	ans += x1 * y2 - x2 * y1
+    x1, y1 = pt[i]
+    x2, y2 = pt[(i + 1) % len(pt)]
+    ans += x1 * y2 - x2 * y1
 ans = abs(ans) // 2 + p // 2 + 1
 
 print(ans)
