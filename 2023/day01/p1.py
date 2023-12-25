b2ecd91991405d4b5f26ac644b9ac72a012b757e
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-12-25 22:51:46 +0900

    Update p1.py

--- 2023/day01/p1.py
+++ 2023/day01/p1.py
@@ -4,8 +4,8 @@ lines = sys.stdin.read().split("\n")
 
 ans = 0
 for line in lines:
-	fir = next(x for x in line if x.isdigit())
-	lst = next(x for x in line[::-1] if x.isdigit())
-	ans += int(fir) * 10 + int(lst)
+    fir = next(x for x in line if x.isdigit())
+    lst = next(x for x in line[::-1] if x.isdigit())
+    ans += int(fir) * 10 + int(lst)
 
 print(ans)
