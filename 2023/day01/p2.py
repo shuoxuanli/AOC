48b108dce1a3e442134515944387ec5e44fae88a
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-12-25 22:52:40 +0900

    Update p2.py

--- 2023/day01/p2.py
+++ 2023/day01/p2.py
@@ -6,8 +6,8 @@ num = "0 zero 1 one 2 two 3 three 4 four 5 five 6 six 7 seven 8 eight 9 nine".sp
 
 ans = 0
 for line in lines:
-	fir = min(num, key = lambda x : line.index(x) if x in line else len(line))
-	lst = min(num, key = lambda x : line[::-1].index(x[::-1]) if x in line else len(line))
-	ans += num.index(fir) // 2 * 10 + num.index(lst) // 2
+    fir = min(num, key = lambda x : line.index(x) if x in line else len(line))
+    lst = min(num, key = lambda x : line[::-1].index(x[::-1]) if x in line else len(line))
+    ans += num.index(fir) // 2 * 10 + num.index(lst) // 2
 
 print(ans)
