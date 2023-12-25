b9f95d1ed415a969a336a936d317e0fc52c7190d
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-12-25 23:28:38 +0900

    Update p1.py

--- 2023/day15/p1.py
+++ 2023/day15/p1.py
@@ -3,9 +3,9 @@ import sys
 line = sys.stdin.read().split(",")
 
 def hash(s):
-	h = 0
-	for x in s:
-		h = (h + ord(x)) * 17 % 256
-	return h
+    h = 0
+    for x in s:
+        h = (h + ord(x)) * 17 % 256
+    return h
 
 print(sum(map(hash, line)))
