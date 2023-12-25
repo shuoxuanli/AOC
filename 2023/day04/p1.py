3d64aed4cd2254646f7b0b0447c16289fadaed6b
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-12-25 23:12:25 +0900

    Update p1.py

--- 2023/day04/p1.py
+++ 2023/day04/p1.py
@@ -4,10 +4,10 @@ lines = sys.stdin.read().split("\n")
 
 ans = 0
 for line in lines:
-	line = line.split(": ")[1]
-	a, b = line.split("|")
-	a, b = a.split(), b.split()
-	s = sum(x in a for x in b)
-	if s > 0: ans += 2 ** (s - 1)
+    line = line.split(": ")[1]
+    a, b = line.split("|")
+    a, b = a.split(), b.split()
+    s = sum(x in a for x in b)
+    if s > 0: ans += 2 ** (s - 1)
 
 print(ans)
