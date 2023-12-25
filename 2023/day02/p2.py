9e30536fe6ed0cf6f365ca673f23efb91094121f
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-12-25 22:53:38 +0900

    Update p2.py

--- 2023/day02/p2.py
+++ 2023/day02/p2.py
@@ -4,14 +4,14 @@ lines = sys.stdin.read().split("\n")
 
 ans = 0
 for line in lines:
-	ID, sets = line.split(": ")
-	r, g, b = 0, 0, 0
-	for x in sets.split("; "):
-		nx = [i.split() for i in x.split(", ")]
-		mp = {b : int(a) for a, b in nx}
-		r = max(r, mp.get("red", 0))
-		g = max(g, mp.get("green", 0))
-		b = max(b, mp.get("blue", 0))
-	ans += r * g * b
+    ID, sets = line.split(": ")
+    r, g, b = 0, 0, 0
+    for x in sets.split("; "):
+        nx = [i.split() for i in x.split(", ")]
+        mp = {b : int(a) for a, b in nx}
+        r = max(r, mp.get("red", 0))
+        g = max(g, mp.get("green", 0))
+        b = max(b, mp.get("blue", 0))
+    ans += r * g * b
 
 print(ans)
