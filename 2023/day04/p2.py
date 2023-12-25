80b6988c9c1f4e1f5678e4a59a955adde58ab759
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-12-25 23:12:34 +0900

    Update p2.py

--- 2023/day04/p2.py
+++ 2023/day04/p2.py
@@ -4,11 +4,11 @@ lines = sys.stdin.read().split("\n")
 
 l = [1] * len(lines)
 for i, line in enumerate(lines):
-	line = line.split(": ")[1]
-	a, b = line.split("|")
-	a, b = a.split(), b.split()
-	s = sum(x in a for x in b)
-	for j in range(1, s + 1):
-		l[i + j] += l[i]
+    line = line.split(": ")[1]
+    a, b = line.split("|")
+    a, b = a.split(), b.split()
+    s = sum(x in a for x in b)
+    for j in range(1, s + 1):
+        l[i + j] += l[i]
 
 print(sum(l))
